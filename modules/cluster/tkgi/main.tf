e6827b6779ac02557739310843cd0002901caf31
Chris Phillipson <cphillipson@pivotal.io>
2021-02-04 06:54:07 -0800

    Fix issue: Terraform configuration must be valid before initialization

--- modules/cluster/tkgi/main.tf
+++ modules/cluster/tkgi/main.tf
@@ -20,6 +20,8 @@ resource "null_resource" "tkgi_cluster" {
   triggers = {
         tags = var.tkgi_tags
         workers = var.tkgi_worker_count
+        tkgi_login = data.local_file.tkgi_login.filename
+        tkgi_apply = data.local_file.tkgi_apply.filename
   }
   provisioner "local-exec" {
     environment = {
@@ -32,8 +34,10 @@ resource "null_resource" "tkgi_cluster" {
       TKGI_WORKER_COUNT = var.tkgi_worker_count
       TKGI_EXTERNAL_HOSTNAME = var.tkgi_external_hostname
       TKGI_TAGS = var.tkgi_tags
+      TKGI_LOGIN_SCRIPT = self.triggers.tkgi_login
+      TKGI_APPLY_SCRIPT = self.triggers.tkgi_apply
     }
-    command = "${data.local_file.tkgi_login.filename} && ${data.local_file.tkgi_apply.filename}"
+    command = "$TKGI_LOGIN_SCRIPT && $TKGI_APPLY_SCRIPT"
   }
 }
 
@@ -44,6 +48,8 @@ resource "null_resource" "tkgi_cluster_destroy" {
       tkgi_password = var.tkgi_password
       tkgi_user = var.tkgi_user
       tkgi_cluster_name = var.tkgi_cluster_name
+      tkgi_login = data.local_file.tkgi_login.filename
+      tkgi_delete = data.local_file.tkgi_delete.filename
   }
   provisioner "local-exec" {
      environment = {
@@ -52,9 +58,11 @@ resource "null_resource" "tkgi_cluster_destroy" {
       TKGI_PASSWORD =  self.triggers.tkgi_password
       TKGI_USER =  self.triggers.tkgi_user
       TKGI_CLUSTER_NAME =  self.triggers.tkgi_cluster_name
+      TKGI_LOGIN_SCRIPT = self.triggers.tkgi_login
+      TKGI_DELETE_SCRIPT = self.triggers.tkgi_delete
     }
     when = destroy
-    command = "${data.local_file.tkgi_login.filename} && ${data.local_file.tkgi_delete.filename}"
+    command = "$TKGI_LOGIN_SCRIPT && $TKGI_DELETE_SCRIPT"
   }
 }
 
@@ -63,6 +71,8 @@ resource "null_resource" "tkgi_cluster_destroy" {
 resource "null_resource" "tkgi_cluster_info" {
   triggers = {
     always_run = timestamp()
+    tkgi_login = data.local_file.tkgi_login.filename
+    tkgi_get = data.local_file.tkgi_get.filename
   }
   provisioner "local-exec" {
      environment = {
@@ -71,8 +81,10 @@ resource "null_resource" "tkgi_cluster_info" {
       TKGI_PASSWORD = var.tkgi_password
       TKGI_USER = var.tkgi_user
       TKGI_CLUSTER_NAME = var.tkgi_cluster_name
+      TKGI_LOGIN_SCRIPT = self.triggers.tkgi_login
+      TKGI_GET_SCRIPT = self.triggers.get_delete
     }
-    command = "${data.local_file.tkgi_login.filename} && ${data.local_file.tkgi_get.filename}"
+    command = "$TKGI_LOGIN_SCRIPT && $TKGI_GET_SCRIPT"
   }
   provisioner "local-exec" {
     when = destroy
